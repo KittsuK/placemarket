725253ac06f1d44b6cb1e4bae836c44bb0367272
KittsuK <pg.verissimo0802@gmail.com>
2024-03-12 16:50:36 -0300

    tem que colocar imagem e arrumar o erro que da no login errado

--- codes/login/script.js
+++ codes/login/script.js
@@ -1,3 +1,51 @@
 function login(){
-    
+    var name = $('#user').val();
+    var password = $('#password').val();
+
+
+    if(name && password && name === "admin" && password === "admin"){
+        const user = {
+            name: name,
+            antryDate: format(new Date()),
+            id: Math.floor(Math.random() * 100000),
+        }
+        localStorage.setItem("name", JSON.stringify(name))
+        window.location.href = "../store/index.html"
+        console.log("deu boa")
+        
+    }else{
+        document.getElementById("error-modal").style.display = "flex";
+        document.getElementById("name").style.border = "2px solid red";
+        document.getElementById("password").style.border = "2 solid red";
+    }
+}
+
+function closeError(){
+    document.getElementById("error-modal").style.display = "none";
+    document.getElementById("user").style.border = "2px solid rgb(118, 0, 145)";
+    document.getElementById("password").style.border = "2px solid rgb(118, 0, 145)";
+}
+
+function showPassword(){
+    var inputPassword = document.getElementById("password");
+    console.log(new Date())
+
+
+
+    if(inputPassword.getAttribute("type") === "password"){
+        inputPassword.setAttribute("type", "text")
+    }else{
+        inputPassword.setAttribute("type", "password")
+    }
+}
+
+function format(item){
+    var options = {
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    }
+    return item.tolocaleString("pt-BR", options)
 }
